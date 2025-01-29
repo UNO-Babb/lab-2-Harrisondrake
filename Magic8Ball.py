9696fa585fd9899e0a0e023d677a6b864ed3f23e
Harrisondrake <harrisondrake@unomaha.edu>
2025-01-29 15:40:41 +0000

    Finished 8ball

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,9 +10,19 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  answers = ["As i see it, yes", "Ask again later", "Better not tell you now",
+             "Don't count on it", "It is certain", "Most likely",
+             "My reply is no", "My sources say no"]
   #Answer question randomly with one of the options from your earlier list.
+  question = input("Ask me a yes or no question: ")
+  num = random.random()
+  num = num *10
+  num = int(num)
+  num = num % 9
+  print(answers[num])
 
+  
 
+  
 if __name__ == '__main__':
   main()
